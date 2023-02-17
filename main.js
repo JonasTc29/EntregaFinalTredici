362216dce8f90611ba390998bdb09991099cd726
JonasT29 <jonastredici@gmail.com>
2023-02-16 22:29:39 -0300

    PreEntrega 1

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
+// FUNCIONES
+
 function mialerta()
 {  
     alert("Reservas únicamente por WhatsApp");
 }
 
-let cantidadError = 0;
-let seleccion = prompt("Bienvenido/a a Suplementos Buildhealth, ¿es usted mayor de 18 años? (SI o NO)");
-
-while (seleccion !== "SI" && seleccion !== "si" &&
-       seleccion !== "NO" && seleccion !== "no")
+function funcionInicial()
 {
-    cantidadError++;
-    if (cantidadError > 3)
+    let cantidadError = 0;
+    let seleccion = prompt("Bienvenido/a a Suplementos Buildhealth, ¿es usted mayor de 18 años? (SI o NO)");
+
+    while (seleccion !== "SI" && seleccion !== "si" &&
+           seleccion !== "NO" && seleccion !== "no")
     {
-        alert ("Te equivocaste demasiadas veces, ¡hasta pronto!");
-        window.location.href = "https://www.google.com/";
-        break;
+        cantidadError++;
+        if (cantidadError > 3)
+        {
+            alert ("Te equivocaste demasiadas veces, ¡hasta pronto!");
+            window.location.href = "https://www.google.com/";
+            break;
+        }
+        alert("Por favor ingrese SI o NO");
+        seleccion = prompt("Bienvenido, ¿es usted mayor de edad? (SI o NO)");
     }
-    alert("Por favor ingrese SI o NO");
-    seleccion = prompt("Bienvenido, ¿es usted mayor de edad? (SI o NO)");
+
+    if (seleccion=== "no" || seleccion === "NO")
+    { 
+        alert ("Gracias por visitar nuestro sitio, ¡hasta pronto!");
+        window.location.href = "https://www.google.com/";
+    }
 }
 
-if (seleccion=== "no" || seleccion === "NO")
-{ 
-    alert ("Gracias por visitar nuestro sitio, ¡hasta pronto!");
-    window.location.href = "https://www.google.com/";
+function calcularTotal()
+{
+    let precios = document.querySelectorAll('.precio'); //selecciono todos los precios de los productos
+    let total = 0;
+    for (let i = 0; i < precios.length; i++) {
+        let precioNumero = parseInt(precios[i].innerHTML.slice(2)); //convierto el valor del precio de string a numero para poder sumarlo
+        total = total + precioNumero;
+    }   
+    let printTotal = "De comprar todos los productos de la web, el valor total del carrito sería de $ " + total;
+    document.getElementById("printTotalCarrito").innerHTML = printTotal;
 }
 
+// CÓDIGO
+
+funcionInicial();
+
+calcularTotal();
